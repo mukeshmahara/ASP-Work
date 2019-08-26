a1693055fec7abc254ac2dc7629b0a5d5bd29e7e
mukesh <mukeshmahara1@gmai.com>
2019-08-27 05:10:37 +0545

    new branch

--- Csharpstudy/Program.cs
+++ Csharpstudy/Program.cs
@@ -4,13 +4,30 @@
     class Program
     {
         static void Main(string[] args){
-        Console.WriteLine("Enter the value of  x :");
-        int x = Convert.ToInt32(Console.ReadLine()); 
-        Console.WriteLine("Enter the value of  y :");
-        int y = Convert.ToInt32(Console.ReadLine()); 
-        int sum = x + y;
-        Console.WriteLine("The sum of two number is :"+sum);
-            
+
+        // Console.WriteLine("Enter the value of  x :");
+        // int x = Convert.ToInt32(Console.ReadLine()); 
+        // Console.WriteLine("Enter the value of  y :");
+        // int y = Convert.ToInt32(Console.ReadLine()); 
+        // int sum = x + y;
+        // Console.WriteLine("The sum of two number is :"+sum);
+
+
+        Methods me = new Methods();
+        
+        Console.Write("Enter the radius of circle : ") ;
+        float rad =float.Parse(Console.ReadLine());
+        Console.WriteLine("The Area of the circle is :"+me.CalculateArea(rad));
+        
+
+        Console.WriteLine("The product of two number is "+ me.multiply(12));
+        
+        (int sum, int product) result = me.ReturnSumAndMul(20,30);
+
+        Console.WriteLine("Tuple Sum: "+result.product);
+        Console.WriteLine("Tuple Product: "+result.sum);
+
+        me.PrintOrderDetails(orderNum:1231123,sellerName:"Mukesh",productCode:"123",price : 1233);
         }
     }
 
